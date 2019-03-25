d70f1caffbd1fcd001735a527491c787e17ac6a2
CUI Hao (Lab) <cuihao.leo@gmail.com>
2019-03-25 17:29:28 +0800

    almost OK

--- app/src/main/java/com/example/stactool/MainActivity.kt
+++ app/src/main/java/com/example/stactool/MainActivity.kt
@@ -9,17 +9,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
-import android.view.View
 import com.androidplot.xy.*
 import com.example.stactool.databinding.ActivityMainBinding
 import org.apache.commons.math3.distribution.*
 import org.apache.commons.math3.exception.MathIllegalArgumentException
 import java.text.DecimalFormat
-import android.graphics.Shader
-import android.graphics.LinearGradient
-import android.graphics.Paint
-import android.support.v4.view.ViewCompat.setAlpha
-
+import java.text.FieldPosition
 
 
 private const val LOG_TAG = "MainActivity"
@@ -52,9 +47,21 @@ class MainActivityViewModel: BaseObservable() {
             Pair(0.000, 0.999),
             Pair(0.010, 0.990)
         )
+
+        val dummyDistribution = object: AbstractRealDistribution() {
+            override fun getSupportUpperBound() = Double.NEGATIVE_INFINITY
+            override fun getSupportLowerBound() = Double.POSITIVE_INFINITY
+            override fun isSupportLowerBoundInclusive() = false
+            override fun isSupportUpperBoundInclusive() = false
+            override fun cumulativeProbability(x: Double) = Double.NaN
+            override fun getNumericalMean() = Double.NaN
+            override fun isSupportConnected() = true
+            override fun getNumericalVariance() = Double.NaN
+            override fun density(x: Double) = Double.NaN
+        }
     }
 
-    fun string2Double(s: String): Double {
+    private fun string2Double(s: String): Double {
         val ts = s.trim()
         return if (ts == "") Double.NaN else ts.toDoubleOrNull()?:Double.NaN
     }
@@ -70,7 +77,7 @@ class MainActivityViewModel: BaseObservable() {
         Double.NaN
     }
 
-    fun getSeriesPDF(nPoints: Int = 100): SimpleXYSeries {
+    private fun getSeriesPDF(nPoints: Int = 100): SimpleXYSeries {
         val xVal: MutableList<Double> = arrayListOf()
         val yVal: MutableList<Double> = arrayListOf()
         val range = plotRightBound - plotLeftBound
@@ -84,12 +91,31 @@ class MainActivityViewModel: BaseObservable() {
         return SimpleXYSeries(xVal, yVal, "")
     }
 
-    fun getSeriesShadow(nPoints: Int = 100): SimpleXYSeries {
+    private fun getSeriesCDF(nPoints: Int = 100): SimpleXYSeries {
+        val xVal: MutableList<Double> = arrayListOf()
+        val yVal: MutableList<Double> = arrayListOf()
+        val range = plotRightBound - plotLeftBound
+        for (i in 0..nPoints) {
+            val x = plotLeftBound + range * i / nPoints
+            val y = _distribution.cumulativeProbability(x)
+            xVal.add(x)
+            yVal.add(y)
+        }
+
+        return SimpleXYSeries(xVal, yVal, "")
+    }
+
+    fun getSeries(nPoints: Int = 100): SimpleXYSeries {
+        return if (plotMode) getSeriesCDF(nPoints) else getSeriesPDF(nPoints)
+    }
+
+    fun getSeries2PDF(nPoints: Int = 100): SimpleXYSeries {
         val end = if (criticalValue != "") {
-            _criticalValue
+            minOf(_criticalValue, plotRightBound)
         } else {
             try {
-                _distribution.inverseCumulativeProbability(_cumulativeProbability)
+                val t = _distribution.inverseCumulativeProbability(_cumulativeProbability)
+                minOf(t, plotRightBound)
             } catch (e: MathIllegalArgumentException) {
                 Double.NaN
             }
@@ -111,6 +137,40 @@ class MainActivityViewModel: BaseObservable() {
         return SimpleXYSeries(xVal, yVal, "")
     }
 
+    fun getSeries2CDF(nPoints: Int = 100): SimpleXYSeries {
+        val pos = if (criticalValue != "") {
+            _criticalValue
+        } else {
+            try {
+                _distribution.inverseCumulativeProbability(_cumulativeProbability)
+            } catch (e: MathIllegalArgumentException) {
+                Double.NaN
+            }
+        }
+
+        val xVal: MutableList<Double> = arrayListOf()
+        val yVal: MutableList<Double> = arrayListOf()
+        if (pos in plotLeftBound..plotRightBound) {
+            xVal.add(pos)
+            yVal.add(_distribution.cumulativeProbability(pos))
+            xVal.add(pos)
+            yVal.add(0.0)
+        }
+
+        return SimpleXYSeries(xVal, yVal, "")
+    }
+
+    fun getSeries2(nPoints: Int = 100): SimpleXYSeries {
+        return if (plotMode) getSeries2CDF(nPoints) else getSeries2PDF(nPoints)
+    }
+
+    @get:Bindable
+    var plotMode: Boolean = false
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.plotMode)
+        }
+
     var distribution: Int = 0
         @Bindable get() = field
         set(value) {
@@ -122,56 +182,82 @@ class MainActivityViewModel: BaseObservable() {
             criticalValue = ""
             notifyChange()
         }
-    val _distribution: AbstractRealDistribution get() = if (distribution == 0) {
-        NormalDistribution(_param1, _param2)
-    } else if (distribution == 1) {
-        TDistribution(_param1)
-    } else if (distribution == 2) {
-        ChiSquaredDistribution(_param1)
-    } else if (distribution == 3) {
-        FDistribution(_param1, _param2)
-    } else {
-        NormalDistribution(0.0, 1.0)  // Just a default
-    }
+    private val _distribution: AbstractRealDistribution
+        get() {
+            try {
+                if (distribution == 0) {
+                    return NormalDistribution(_param1, _param2)
+                } else if (distribution == 1) {
+                    return TDistribution(_param1)
+                } else if (distribution == 2) {
+                    return ChiSquaredDistribution(_param1)
+                } else if (distribution == 3) {
+                    return FDistribution(_param1, _param2)
+                }
+            } catch (e: MathIllegalArgumentException) {
+                ;
+            }
+
+            return dummyDistribution
+        }
+
+    @get:Bindable
+    var paramError: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.paramError)
+        }
 
     var param1: String = ""
         @Bindable get() = field
         set(value) {
             if (field != value) {
                 field = value
-                Log.d(LOG_TAG, String.format("param1 = %s", field))
+                paramError = if (_distribution === dummyDistribution) "Invalid parameter" else null
                 notifyChange()
             }
         }
-    val _param1: Double get() = string2Double(param1)
+    private val _param1: Double get() = string2Double(param1)
 
     var param2: String = ""
         @Bindable get() = field
         set(value) {
             if (field != value) {
                 field = value
-                Log.d(LOG_TAG, String.format("param2 = %s", field))
+                paramError = if (_distribution === dummyDistribution) "Invalid parameter" else null
                 notifyChange()
             }
         }
-    val _param2: Double get() = string2Double(param2)
+    private val _param2: Double get() = string2Double(param2)
 
     var cumulativeProbability: String = ""
         @Bindable get() = field
         set(value) {
             field = value
             notifyPropertyChanged(BR.cumulativeProbability)
-            notifyPropertyChanged(BR.criticalValueHint)
             if (value != "") {
                 criticalValue = ""
                 notifyPropertyChanged(BR.criticalValue)
             }
+            notifyPropertyChanged(BR.criticalValueHint)
+
+            if (_cumulativeProbability in 0.0..1.0 || value == "") {
+                cumulativeProbabilityError = null
+            } else {
+                cumulativeProbabilityError = "Out of range (0.0 to 1.0)"
+            }
         }
-    val _cumulativeProbability: Double get() = string2Double(cumulativeProbability)
+    private val _cumulativeProbability: Double get() = string2Double(cumulativeProbability)
     val cumulativeProbabilityHint: String
         @Bindable get() {
             val t = _distribution.cumulativeProbability(_criticalValue)
-            return "%.4f".format(t)
+            return "%.5f".format(t)
+        }
+    @get:Bindable
+    var cumulativeProbabilityError: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.cumulativeProbabilityError)
         }
 
     var criticalValue: String = ""
@@ -179,14 +265,14 @@ class MainActivityViewModel: BaseObservable() {
         set(value) {
             field = value
             notifyPropertyChanged(BR.criticalValue)
-            notifyPropertyChanged(BR.cumulativeProbabilityHint)
+            notifyPropertyChanged(BR.oneP)
             if (value != "") {
                 cumulativeProbability = ""
                 notifyPropertyChanged(BR.cumulativeProbability)
-                notifyPropertyChanged(BR.cumulativeProbabilityHint)
             }
+            notifyPropertyChanged(BR.cumulativeProbabilityHint)
         }
-    val _criticalValue: Double get() = string2Double(criticalValue)
+    private val _criticalValue: Double get() = string2Double(criticalValue)
     val criticalValueHint: String
         @Bindable get() {
             val t = try {
@@ -194,23 +280,17 @@ class MainActivityViewModel: BaseObservable() {
             } catch (e: MathIllegalArgumentException) {
                 Double.NaN
             }
-            return "%.4f".format(t)
+            return "%.5f".format(t)
         }
 
-    val param1Name: String
-        @Bindable get() = parameterNames[distribution].first
-
-    val param2Name: String
-        @Bindable get() = parameterNames[distribution].second
-
-    val param2Visibility: Int
-        @Bindable get() = if (param2Name == "") View.GONE else View.VISIBLE
-
-    val param1InputType: Int
-        @Bindable get() = InputType.TYPE_CLASS_NUMBER or inputRestrains[distribution].first
-
-    val param2InputType: Int
-        @Bindable get() = InputType.TYPE_CLASS_NUMBER or inputRestrains[distribution].second
+    val oneP: Double
+        @Bindable get() {
+            return try {
+                _distribution.cumulativeProbability(_criticalValue)
+            } catch (e: MathIllegalArgumentException) {
+                Double.NaN
+            }
+        }
 }
 
 class MainActivity: Activity() {
@@ -218,8 +298,14 @@ class MainActivity: Activity() {
 
     val plot by lazy {
         val view = findViewById<XYPlot>(R.id.plot);
-        view.graph.getLineLabelStyle(XYGraphWidget.Edge.LEFT).format = DecimalFormat("0.00")
-        view.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = DecimalFormat("0.00")
+        val formatter = object: DecimalFormat() {
+            override fun format(number: Double, result: StringBuffer, fieldPosition: FieldPosition): StringBuffer {
+                return result.append(String.format("%.2G", number))
+            }
+        }
+        view.setRangeLowerBoundary(0.0, BoundaryMode.FIXED)
+        view.graph.getLineLabelStyle(XYGraphWidget.Edge.LEFT).format = formatter
+        view.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = formatter
         view
     }
 
@@ -230,25 +316,21 @@ class MainActivity: Activity() {
         bindingData.addOnPropertyChangedCallback(object: Observable.OnPropertyChangedCallback()  {
             var series1: XYSeries? = null
             var series2: XYSeries? = null
-            val filling by lazy {
-                val formatter = LineAndPointFormatter(Color.RED, Color.RED, Color.RED, null)
-                val lineFill = Paint()
-                lineFill.alpha = 200
-                lineFill.shader = LinearGradient(0f, 0f, 0f, 250f, Color.WHITE, Color.GREEN, Shader.TileMode.MIRROR)
-                formatter.fillPaint = lineFill
-                formatter
-            }
 
             override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
                 Log.d(LOG_TAG, String.format("Property %d changed!", propertyId))
 
-                if (propertyId == BR._all || propertyId == BR.distribution || propertyId == BR.param1 || propertyId == BR.param2) {
+                if (propertyId == BR._all || propertyId == BR.distribution || propertyId == BR.param1 || propertyId == BR.param2 || propertyId == BR.plotMode) {
                     if (bindingData.plotLeftBound < bindingData.plotRightBound) {
-                        series1 ?: plot.removeSeries(series1)
-                        series2 ?: plot.removeSeries(series2)
+                        if (series1 != null)  {
+                            plot.removeSeries(series1)
+                        }
+                        if (series2 != null)  {
+                            plot.removeSeries(series2)
+                        }
 
-                        series1 = bindingData.getSeriesPDF()
-                        series2 = bindingData.getSeriesShadow()
+                        series1 = bindingData.getSeries()
+                        series2 = bindingData.getSeries2()
 
                         plot.addSeries(
                             series1,
@@ -257,20 +339,28 @@ class MainActivity: Activity() {
 
                         plot.addSeries(
                             series2,
-                            LineAndPointFormatter(Color.RED, null, Color.RED, null)
+                            LineAndPointFormatter(0x7FFF851B, null, 0x7FFF851B, null)
                         )
 
                         plot.setDomainBoundaries(bindingData.plotLeftBound, bindingData.plotRightBound, BoundaryMode.AUTO)
+                        plot.redraw()
+                    } else {
+                        series1 = null
+                        series2 = null
+                        plot.clear()
                         plot.redraw()
                     }
                 }
 
                 if (propertyId == BR.criticalValueHint || propertyId == BR.cumulativeProbabilityHint) {
-                    series2 ?: plot.removeSeries(series2)
-                    series2 = bindingData.getSeriesShadow()
+                    if (series2 != null)  {
+                        plot.removeSeries(series2)
+                    }
+
+                    series2 = bindingData.getSeries2()
                     plot.addSeries(
                         series2,
-                        LineAndPointFormatter(Color.RED, null, Color.RED, null)
+                        LineAndPointFormatter(0x7FFF851B, null, 0x7FFF851B, null)
                     )
                     plot.redraw()
                 }
